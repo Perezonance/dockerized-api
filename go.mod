87564d5d9514db6f5f6cdfe55aa919f42aee8fd2
manuel.parez <manuel.parez@toyota.com>
2020-02-07 15:45:00 -0600

    added zap logger dependency

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/alexperez/dockerized-api
 
 go 1.13
+
+require go.uber.org/zap v1.13.0
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"go.uber.org/zap"
 )
 
 type server struct {}
@@ -16,4 +17,11 @@ func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/json")
 	w.Write([]byte(`{"message": "Hello, world, from Docker!"}`))
+}
+
+func getLogger(logProd bool) (*zap.Logger, error) {
+	if logProd {
+		return zap.NewProduction()
+	}
+	return zap.NewDevelopment()
 }
